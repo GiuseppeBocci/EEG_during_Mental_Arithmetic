e0dabd75eec4970bdde16963e501e934cff2a189
Giuseppe <peppebocci13@gmail.com>
2023-10-13 00:33:19 +0200

    band subdivision updated

--- Band_subdivision.m
+++ Band_subdivision.m
@@ -1,7 +1,7 @@
 clear; close all; clc;
 
 % import data
-path_out = "Risultati\Bande di frequenza";
+path_out = "bands";
 listofsubjects = dir("Data");
 lsubj_1 = find(contains({listofsubjects.name}, '_1'));
 listofsubjects_1 = listofsubjects(lsubj_1,:);
@@ -10,30 +10,54 @@ listofsubjects_2 = listofsubjects(lsubj_2,:);
 
 load("filters.mat");
 
+% initialization of variables
 coefficienti_Bande= struct('delta',  filter_delta.Coefficients, ...
     'theta',  filter_theta.Coefficients,...
     'alpha',  filter_alpha.Coefficients,...
     'beta',  filter_beta.Coefficients);
-fields_Bande= fieldnames(coefficienti_Bande);
+fields_bande= fieldnames(coefficienti_Bande);
 
-bande_EEG= struct();
+bande_EEG_1 = struct();
+bande_EEG_2 = struct();
 
 
 % filter every signal and channels in each band
+
 % --- subjects in rest ---
-for s= 1:numel(listofsubjects_1)
-    EEG= load(['Data/' listofsubjects(s).name]);
+for s = 1:numel(listofsubjects_1)
+    
+    EEG = load(['Data/' listofsubjects_1(s).name]);
             Channels= fieldnames(EEG);
 
-    for ch=1:numel(Channels)
+    for ch = 1:numel(Channels)
         %BANDS EXTRACTION 
-        for banda=1:numel(fields_Bande)
+        for banda=1:numel(fields_bande)
             % every row has a subject
-            EEG_temp{s,banda}= filter(coefficienti_Bande.(fields_Bande{banda}),1,EEG.(Channels{ch}));
+            EEG_temp{s,banda}= filter(coefficienti_Bande.(fields_bande{banda}),1,EEG.(Channels{ch}));
             % TODO: filtfilt
         end
-        bande_EEG.(Channels{ch})=EEG_temp;
+        bande_EEG_1.(Channels{ch})=EEG_temp;
     end
-    % save(strcat(path_out, '\subject_', num2str(s), '_Bande_baseline.mat'), 'Bande_EEG')
 end
+
+save('original_band_1.mat',"bande_EEG_1");
+
+% --- subjects during task ---
+for s = 1:numel(listofsubjects_2)
+    
+    EEG = load(['Data/' listofsubjects_2(s).name]);
+            Channels= fieldnames(EEG);
+
+    for ch = 1:numel(Channels)
+        %BANDS EXTRACTION 
+        for banda=1:numel(fields_bande)
+            % every row has a subject
+            EEG_temp{s,banda}= filter(coefficienti_Bande.(fields_bande{banda}),1,EEG.(Channels{ch}));
+            % TODO: filtfilt
+        end
+        bande_EEG_2.(Channels{ch})=EEG_temp;
+    end
+end
+
+save('original_band_2.mat',"bande_EEG_2");
 
