41dca83a17294a0ea3d809beabe1d1fdef8f362c
Lolleeee <101202956+Lolleeee@users.noreply.github.com>
2023-10-16 17:25:36 +0200

    Update Band_sub.m
    Ora funziona anche con matrici SOS dei filtri IIR

--- Band_sub.m
+++ Band_sub.m
@@ -1,11 +1,11 @@
-function [bande_EEG_1,bande_EEG_2] = Band_sub(coefficienti_Bande)
+function [bande_EEG_1,bande_EEG_2] = Band_sub(coefficienti_Bande, new_fs, old_fs)
     % import data
     listofsubjects = dir("Data");
     lsubj_1 = find(contains({listofsubjects.name}, '_1'));
     listofsubjects_1 = listofsubjects(lsubj_1,:);
     lsubj_2 = find(contains({listofsubjects.name}, '_2'));
     listofsubjects_2 = listofsubjects(lsubj_2,:);
-
+    
     % initialization of variables
     bande_EEG_1 = struct();
     bande_EEG_2 = struct();
@@ -17,14 +17,21 @@ function [bande_EEG_1,bande_EEG_2] = Band_sub(coefficienti_Bande)
     for s = 1:numel(listofsubjects_1)
         
         EEG = load(['Data/' listofsubjects_1(s).name]);
-                Channels= fieldnames(EEG);
+        
+        Channels= fieldnames(EEG);
     
         for ch = 1:numel(Channels)
             %BANDS EXTRACTION 
+            EEG.(Channels{ch})=resample(EEG.(Channels{ch}),new_fs,old_fs);
             for banda=1:numel(fields_bande)
                 % every row has a subject
-                EEG_temp{s,banda}= filter(coefficienti_Bande.(fields_bande{banda}),1,EEG.(Channels{ch}));
-                % TODO: filtfilt
+                [dim1,~] = size(coefficienti_Bande.(fields_bande{banda}));
+                if dim1 == 1
+                    EEG_temp{s,banda}= filter(coefficienti_Bande.(fields_bande{banda}),1,EEG.(Channels{ch}));
+                    % TODO: filtfilt
+                else
+                    EEG_temp{s,banda}= sosfilt(coefficienti_Bande.(fields_bande{banda}),EEG.(Channels{ch}));
+                end
             end
             bande_EEG_1.(Channels{ch})=EEG_temp;
         end
@@ -34,14 +41,20 @@ function [bande_EEG_1,bande_EEG_2] = Band_sub(coefficienti_Bande)
     for s = 1:numel(listofsubjects_2)
         
         EEG = load(['Data/' listofsubjects_2(s).name]);
-                Channels= fieldnames(EEG);
+        Channels= fieldnames(EEG);
     
         for ch = 1:numel(Channels)
             %BANDS EXTRACTION 
+            EEG.(Channels{ch})=resample(EEG.(Channels{ch}),new_fs,old_fs);
             for banda=1:numel(fields_bande)
                 % every row has a subject
-                EEG_temp{s,banda}= filter(coefficienti_Bande.(fields_bande{banda}),1,EEG.(Channels{ch}));
-                % TODO: filtfilt
+                [dim1,~] = size(coefficienti_Bande.(fields_bande{banda}));
+                if dim1 == 1
+                    EEG_temp{s,banda}= filter(coefficienti_Bande.(fields_bande{banda}),1,EEG.(Channels{ch}));
+                    % TODO: filtfilt
+                else
+                    EEG_temp{s,banda}= sosfilt(coefficienti_Bande.(fields_bande{banda}),EEG.(Channels{ch}));
+                end
             end
             bande_EEG_2.(Channels{ch})=EEG_temp;
         end
